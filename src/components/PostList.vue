4038e482004a1337b846eb2acc427227dda806bf
Dusan <dusankubala1996@gmail.com>
2021-08-10 13:23:47 +0200

    Composition API

---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-list">
+      <div v-for="post in posts" :key="post.id">
+          <SinglePost :post="post" />
+      </div>
+  </div>
+</template>
+
+<script>
+import SinglePost from '../components/SinglePost.vue'
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
+
+export default {
+    props: [ 'posts' ],
+    components: { SinglePost },
+    setup(props) {
+        onMounted(() => console.log('component mounted'))
+        onUnmounted(() => console.log('component UNmounted'))
+        onUpdated(() => console.log('component updated'))
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="home">
+    <h1>Home</h1>
+    <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">Toggle posts</button>
+    <button @click="posts.pop()">Delete post</button>
   </div>
 </template>
 
 <script>
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      { title: 'Welcome to blog', body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', id: 1},
+      { title: 'Top 5 CSS tips', body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', id: 2}
+    ])
+
+    const showPosts = ref(true)
+
+    return { posts, showPosts }
+  }
 }
 </script>
